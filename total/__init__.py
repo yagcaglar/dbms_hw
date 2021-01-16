9af5e56b8c64f525af760e46c44029bd7581e70e
yagcaglar <56485890+yagcaglar@users.noreply.github.com>
2021-01-16 18:54:24 +0300

    Update __init__.py

--- total/__init__.py
+++ total/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import psycopg2
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-con = psycopg2.connect(dbname="website", user="postgres", password="8748")
+db_url=os.environ.get("DATABASE_URL")
+con = psycopg2.connect(db_url, sslmode='require')
 cur = con.cursor()
 
 bcrypt = Bcrypt(app)
