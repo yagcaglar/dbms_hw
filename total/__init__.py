d37119e6781d47a0244ae137c0a696c92282b2e9
yagcaglar <56485890+yagcaglar@users.noreply.github.com>
2021-01-16 20:18:15 +0300

    Update __init__.py

--- total/__init__.py
+++ total/__init__.py
@@ -16,5 +16,5 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 from total import routes
-from total import datab
-create_db()
+#from total import datab
+#create_db()
