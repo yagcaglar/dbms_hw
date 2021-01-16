00ed29e87b63b3f7ae74b817a1ec6519cd1d574c
yagcaglar <yagcaglar@gmail.com>
2021-01-16 22:40:48 +0300

    new

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from server import app
+import server
+
+app= server.create_app()
 
 if __name__ =='__main__':
 	app.run(debug=True)
--- server.py
+++ server.py
@@ -4,15 +4,20 @@ from flask_login import LoginManager
 import psycopg2
 import os
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-db_url=os.environ.get("DATABASE_URL")
-con = psycopg2.connect(db_url, sslmode='require')
-cur = con.cursor()
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    db_url=os.environ.get("DATABASE_URL")
+    con = psycopg2.connect(db_url, sslmode='require')
+    cur = con.cursor()
 
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+    bcrypt = Bcrypt(app)
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'login'
+    login_manager.login_message_category = 'info'
 
-from total import routes
+    return app
+
+if name == "main":
+    app=create_app()
+    app.run(host="0.0.0.0", port=8080)
